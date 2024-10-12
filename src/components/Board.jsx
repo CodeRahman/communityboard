816bfd097f72f0da60dd67d2a6a972e30bd1595f
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2024-10-12 02:31:16 -0500

    json file add

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Subboard from './Subboard.jsx'
+import gamesData from './games.json'; // Import the games.json file
 
 const Board = () => {
 
     return (
         <div className="Board">
-            <Subboard image='https://shorturl.at/Dcckx' 
-            gamename='at ULM' sport="Men's Football" date="4pm, Oct 12, 2024" links="https://southernmiss.com/sports/football" />
-
-            <Subboard image='https://shorturl.at/vLjfA' 
-            gamename='at Arkansas State' sport="Women's Soccer" date="1pm, Oct 13, 2024" links="https://southernmiss.com/sports/womens-soccer" />
+            {gamesData.map((game, index) => (
+                <Subboard
+                    key={index}
+                    image={game.image}
+                    gamename={game.gamename}
+                    sport={game.sport}
+                    date={game.date}
+                    links={game.links}
+                />
+            ))}
         </div>
          
     )
