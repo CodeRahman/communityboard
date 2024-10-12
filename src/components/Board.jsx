6815de80911afc25db8bdf5c0d48447ebf569e0f
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2024-10-12 02:22:06 -0500

    up

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,10 +5,11 @@ const Board = () => {
 
     return (
         <div className="Board">
-            
-
             <Subboard image='https://shorturl.at/Dcckx' 
-            gamename='vs ULM' sport="Men's Football" date="TBA, Oct 12, 2024" links="https://southernmiss.com/sports/football" />
+            gamename='at ULM' sport="Men's Football" date="4pm, Oct 12, 2024" links="https://southernmiss.com/sports/football" />
+
+            <Subboard image='https://shorturl.at/vLjfA' 
+            gamename='at Arkansas State' sport="Women's Soccer" date="1pm, Oct 13, 2024" links="https://southernmiss.com/sports/womens-soccer" />
         </div>
          
     )
