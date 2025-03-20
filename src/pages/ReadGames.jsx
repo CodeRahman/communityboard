cc9670d16fb117ed41337e6ffdcfa800796f248a
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2025-03-20 13:42:47 -0500

    update to backend

--- src/pages/ReadGames.jsx
+++ src/pages/ReadGames.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Board from '../components/Board';
-import gamesData from '../components/games.json'; // Import the games.json file
 
 const ReadGames = () => {
 
@@ -8,7 +7,7 @@ const ReadGames = () => {
         <div className="ReadGames">
             <img className="usmlogo"src='https://dxbhsrqyrr690.cloudfront.net/sidearm.nextgen.sites/southernmiss.com/images/nextgen_2023/main_nav_logo.svg'></img>
             <p className='pagename'>Southern Miss Games</p>
-            <Board games = {gamesData}/>
+            <Board/>
         </div>
     )
 }
